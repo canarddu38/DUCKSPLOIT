03b187cda7cef7206948abdc954ef575cbcc84fc
Canarddu38 <Jules.canardcoin@gmail.com>
2022-01-21 23:59:59 +0100

    1.0.1.9

--- SERVER/scripts/setup2.cmd
+++ SERVER/scripts/setup2.cmd
@@ -66,3 +66,69 @@ echo [o] Finished!
 pause
 
 color F
+
+
+
+
+::move commands\*.* C:\Windows\System32
+::copy commands\*.txt C:\Windows\System32
+@echo off
+
+cls
+
+color C
+
+set current_dir=%~dp0
+set server_dir=%current_dir:\scripts=%
+set root_dir=%server_dir:\SERVER=%
+
+echo server: %server_dir%
+echo current: %current_dir%
+echo root: %root_dir%
+
+cd %server_dir%
+
+for /f %%i in (version.txt) do SET version=%%i
+
+echo.
+set /p result="Create a menu shortcut? [y/n]"
+
+if %result%==y (
+echo.
+echo [+] Installing Menu Shortcut...
+
+if exist C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Ducksploit\ (
+copy %server_dir%\ducksploit.bat "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Ducksloit\"
+) else (
+cd "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\"
+mkdir Ducksploit
+copy %server_dir%\ducksploit.bat "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Ducksploit\"
+cd %server_dir%
+)
+
+
+echo.
+set /p result2="Install Ducksploit v.%version% ...? [y/n]"
+
+if %result2%==y (
+echo.
+echo [+] Installing Ducksploit v.%version% ...
+
+
+copy %server_dir%\ducksploit.bat "C:\Windows\System32"
+@echo on
+move %server_dir%\ducksploit.bat C:\Windows\System32
+pause
+copy %server_dir%\DATA\ducksploit.bat %server_dir%
+
+
+
+) else (
+echo [x] Cancelled
+)
+
+echo.
+echo [o] Finished!
+pause
+
+color F
