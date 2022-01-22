40f9d407aad7a2b1ceaedd5af509a7706539fcd0
Canarddu38 <Jules.canardcoin@gmail.com>
2022-01-22 15:25:56 +0100

    v1.0.2.1

--- SERVER/commands/update.bat
+++ SERVER/commands/update.bat
@@ -2,4 +2,4 @@
 wget wget -O C:\Users\%username% https://github.com/canarddu38/DUCKSPLOIT/archive/root.zip
 
 cd C:\Users\%username%
-start commandshttps://github.com/canarddu38/DUCKSPLOIT/archive/root.zip\sudoupdate.bat.lnk
+start https://github.com/canarddu38/DUCKSPLOIT/archive/root.zip\sudoupdate.bat.lnk
