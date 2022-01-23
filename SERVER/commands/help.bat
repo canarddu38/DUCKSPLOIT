ab5dc4f46aa3bcd39785cd712c1235d400e2f30f
Canarddu38 <Jules.canardcoin@gmail.com>
2022-01-23 16:37:12 +0100

    v1.0.2.5

--- SERVER/commands/help.bat
+++ SERVER/commands/help.bat
@@ -12,14 +12,17 @@ echo    [6] Mysc
 echo.
 
 set /p input="Choose option: [int] "
-
-if %input%==0 (for /f %%i in (C:\Users\%username%\DUCKSPLOIT\help\config.txt) do echo %%i&&pause&&sudoducksploit) else (
-if %input%==1 (for /f %%i in (C:\Users\%username%\DUCKSPLOIT\help\exploits.txt) do echo %%i&&pause&&sudoducksploit) else (
-if %input%==2 (for /f %%i in (C:\Users\%username%\DUCKSPLOIT\help\mouse.txt) do echo %%i&&pause&&sudoducksploit) else (
-if %input%==3 (for /f %%i in (C:\Users\%username%\DUCKSPLOIT\help\spy.txt) do echo %%i&&pause&&sudoducksploit) else (
-if %input%==4 (for /f %%i in (C:\Users\%username%\DUCKSPLOIT\help\mysc.txt) do echo %%i&&pause&&sudoducksploit) else (
+if %input%==0 (for /f %%i in (C:\Users\%username%\DUCKSPLOIT\help\account.txt) do echo %%i&&pause&&sudoducksploit) else (
+if %input%==1 (for /f %%i in (C:\Users\%username%\DUCKSPLOIT\help\config.txt) do echo %%i&&pause&&sudoducksploit) else (
+if %input%==2 (for /f %%i in (C:\Users\%username%\DUCKSPLOIT\help\exploits.txt) do echo %%i&&pause&&sudoducksploit) else (
+if %input%==3 (for /f %%i in (C:\Users\%username%\DUCKSPLOIT\help\mouse.txt) do echo %%i&&pause&&sudoducksploit) else (
+if %input%==4 (for /f %%i in (C:\Users\%username%\DUCKSPLOIT\help\spy.txt) do echo %%i&&pause&&sudoducksploit) else (
+if %input%==5 (for /f %%i in (C:\Users\%username%\DUCKSPLOIT\help\troll.txt) do echo %%i&&pause&&sudoducksploit) else (
+if %input%==6 (for /f %%i in (C:\Users\%username%\DUCKSPLOIT\help\mysc.txt) do echo %%i&&pause&&sudoducksploit) else (
 echo [x] Invalid Arg
 goto help
+)
+)
 )
 )
 )
