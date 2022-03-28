ce2fdd73cf8de009f4e58ba50b03bc0d825d7afd
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-03-28 19:01:13 +0200

    Add files via upload

---
+++ hacker/setup-notif.ps1
@@ -0,0 +1,44 @@
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+Add-Type -AssemblyName System.Windows.Forms
+
+function Show-Notification {
+    [cmdletbinding()]
+    Param (
+        [string]
+        $ToastTitle,
+        [string]
+        [parameter(ValueFromPipeline)]
+        $ToastText
+    )
+
+    [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
+    $Template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText02)
+
+    $RawXml = [xml] $Template.GetXml()
+    ($RawXml.toast.visual.binding.text|where {$_.id -eq "1"}).AppendChild($RawXml.CreateTextNode($ToastTitle)) > $null
+    ($RawXml.toast.visual.binding.text|where {$_.id -eq "2"}).AppendChild($RawXml.CreateTextNode($ToastText)) > $null
+
+    $SerializedXml = New-Object Windows.Data.Xml.Dom.XmlDocument
+    $SerializedXml.LoadXml($RawXml.OuterXml)
+	
+	$Icon = 'Info'
+    
+	$Toast = [Windows.UI.Notifications.ToastNotification]::new($SerializedXml)
+    $Toast.Tag = "Information"
+    $Toast.Group = "Information"
+	$Toast.Icon = New-Object system.drawing.icon ("C:\DuckSploit\images\icon.ico")
+	$Toast.BalloonTipIcon = $Icon
+	
+	
+	
+	# $balloonToolTip = New-Object System.Windows.Forms.NotifyIcon;
+	# $balloonToolTip.Icon = New-Object system.drawing.icon ("C:\DuckSploit\images\icon.ico")
+    # $balloonToolTip.BalloonTipIcon = $Icon
+	
+    $Toast.ExpirationTime = [DateTimeOffset]::Now.AddMinutes(1)
+
+    $Notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("DuckSploit")
+    $Notifier.Show($Toast);
+}
+$version = Get-Content C:\DuckSploit\version.txt
+Show-Notification "DuckpvpTeam" ("Thanks you for Downloading DUCKSPLOIT " + $version)
