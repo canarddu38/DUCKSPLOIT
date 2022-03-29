f8e744f0fd9c126c7673c60451d76fda7f15c77a
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-03-29 22:30:29 +0200

    Add files via upload

--- docs/get.js
+++ docs/get.js
@@ -1,3 +1,22 @@
+onst getDeviceType = () => {
+  const ua = navigator.userAgent;
+  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+    return "tablet";
+  }
+  if (
+    /Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+      ua
+    )
+  ) {
+    return "mobile";
+  }
+  return "desktop";
+};
+
+
+
+
+
 // As with JSON, use the Fetch API & ES6
 fetch('https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/version.txt')
   .then(response => response.text())
