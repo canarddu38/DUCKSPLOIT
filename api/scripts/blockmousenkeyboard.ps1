58520c0c9e0f47183c8212be0e3e29d624acf86a
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-05-26 11:57:16 +0200

    Create blockmousenkeyboard.ps1

---
+++ api/scripts/blockmousenkeyboard.ps1
@@ -0,0 +1,17 @@
+#Run As Administrator
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+$code = @"
+    [DllImport("user32.dll")]
+    public static extern bool BlockInput(bool fBlockIt);
+"@
+
+$userInput = Add-Type -MemberDefinition $code -Name UserInput -Namespace UserInput -PassThru
+
+function Disable-UserInput($seconds) {
+    $userInput::BlockInput($true)
+    Start-Sleep $seconds
+    $userInput::BlockInput($false)
+}
+
+Disable-UserInput -seconds 20 | Out-Null
