543b999b52c352c84e46abf9cbd28dc309d2447b
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-06-02 12:33:33 +0200

    Create desktop_stream.ps1

---
+++ api/scripts/desktop_stream.ps1
@@ -0,0 +1,30 @@
+Add-Type -AssemblyName System.Windows.Forms
+$screen_cnt  = [System.Windows.Forms.Screen]::AllScreens.Count
+$col_screens = [system.windows.forms.screen]::AllScreens
+
+$info_screens = ($col_screens | ForEach-Object {
+$width = "$($_.Bounds.Width)"
+$height = "$($_.Bounds.Height)"
+}
+)
+
+
+
+[Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+function screenshot([Drawing.Rectangle]$bounds, $path) {
+   $bmp = New-Object Drawing.Bitmap $bounds.width, $bounds.height
+   $graphics = [Drawing.Graphics]::FromImage($bmp)
+
+   $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+
+   $bmp.Save($path)
+
+   $graphics.Dispose()
+   $bmp.Dispose()
+}
+
+# dimensions
+$bounds = [Drawing.Rectangle]::FromLTRB(0, 0, $width, $height)
+# output_path
+screenshot $bounds "./last.jpg"
+Write-Host "Done!"
