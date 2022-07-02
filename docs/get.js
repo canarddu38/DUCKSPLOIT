82b0e750647740ba7f64aad54335fd0f550c10ed
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-07-02 19:44:54 +0200

    Add files via upload

--- docs/get.js
+++ docs/get.js
@@ -1,35 +1,41 @@
-var detectedOS = "Unknown OS";
-var versionurl = "https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/version.txt"
-var downloadurl = "nothin";
+var detectedOS = "Windows";
+var versionurl = "https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/windows/version.txt"
 
-if (navigator.appVersion.indexOf("Android") != -1) {
-    detectedOS = "Android";
-	versionurl = "https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/android/version.txt";
-	downloadurl = "https://github.com/canarddu38/DUCKSPLOIT/raw/root/hacker/android/DSinstaller.apk";
-	alert(downloadurl);
-	
-} else if (navigator.appVersion.indexOf("Win") != -1) {
-    detectedOS = "Windows";
+function getOS() {
+  var userAgent = window.navigator.userAgent,
+      platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
+      macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+      windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+      iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+      os = null;
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = 'Mac OS (unavailiable)';
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = 'iOS (unavailiable)';
 	versionurl = "https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/windows/version.txt";
-	downloadurl = "https://github.com/canarddu38/DUCKSPLOIT/raw/root/hacker/windows/DSinstaller.exe";
-	alert(downloadurl);
-	
-} else if (navigator.appVersion.indexOf("Linux") != -1) {
-    detectedOS = "Linux";
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = 'Windows';
+	versionurl = "https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/windows/version.txt";
+  } else if (/Android/.test(userAgent)) {
+    os = 'Android';
+	versionurl = "https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/android/version.txt";
+  } else if (/Linux/.test(platform)) {
+    os = 'Linux';
 	versionurl = "https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/linux/version.txt";
-	downloadurl = "https://github.com/canarddu38/DUCKSPLOIT/raw/root/hacker/linux/DSinstaller.deb";
-	alert(downloadurl);
-}
-	
-alert(detectedOS);
+  }
 
+  return os;
+}
+
+detectedOS = getOS();
 
 
 fetch(versionurl)
   .then(response => response.text())
   .then(data => {
-  document.getElementById("version").innerHTML = 'last version: V'.concat(data);
-  document.getElementById("downloadlastbut").href=downloadurl;
+  var message = 'last version ('.concat(detectedOS);
+  document.getElementById("version").innerHTML = message.concat("): ".concat(data));
   });
   
   
