2304a80f67f06c1fc8b6a56240130f1b2d14584c
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-07-06 15:07:14 +0200

    Create skull.bat

---
+++ api/troll/skull.bat
@@ -0,0 +1,28 @@
+@echo off
+
+mode con:cols=41 lines=21
+
+color a
+
+echo      NO!                          MNO!    
+echo     MNO!!     [DUCKSPLOIT]        MNNOO!
+echo   MMNO!                           MNNOO!!
+echo MNOONNOO!   MMMMMMMMMMPPPOII!   MNNO!!!!
+echo !O! NNO! MMMMMMMMMMMMMPPPOOOII!! NO!
+echo       ! MMMMMMMMMMMMMPPPPOOOOIII! !
+echo        MMMMMMMMMMMMPPPPPOOOOOOII!!
+echo        MMMMMOOOOOOPPPPPPPPOOOOMII!
+echo        MMMMM..    OPPMMP    .,OMI!
+echo        MMMM::   o.,OPMP,.o   ::I!!
+echo          NNM:::.,,OOPM!P,.::::!!
+echo         MMNNNNNOOOOPMO!!IIPPO!!O!
+echo         MMMMMNNNNOO:!!:!!IPPPPOO!
+echo          MMMMMNNOOMMNNIIIPPPOO!!
+echo             MMMONNMMNNNIIIOO!
+echo           MN MOMMMNNNIIIIIO! OO
+echo          MNO! IiiiiiiiiiiiI OOOO
+echo     NNN.MNO!   O!!!!!!!!!O   OONO NO!
+echo    MNNNNNO!    OOOOOOOOOOO    MMNNON!
+echo      MNNNNO!    PPPPPPPPP    MMNON!
+echo         OO!                   ON!
+pause
