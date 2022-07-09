8bfc717331b71716a05a888dc0641d493aff6447
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-07-09 12:39:12 +0200

    Add files via upload

---
+++ docs/download/get2.js
@@ -0,0 +1,20 @@
+fetch("https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/windows/version.txt")
+  .then(response => response.text())
+  .then(data1 => {
+  document.getElementById("windows_version").innerHTML = data1;
+  });
+  
+fetch("https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/linux/version.txt")
+  .then(response => response.text())
+  .then(data2 => {
+  document.getElementById("linux_version").innerHTML = data2;
+  });
+  
+fetch("https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/android/version.txt")
+  .then(response => response.text())
+  .then(data3 => {
+  document.getElementById("android_version").innerHTML = data3;
+  });
+  
+  
+
