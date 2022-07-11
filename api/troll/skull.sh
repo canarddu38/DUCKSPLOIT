b392897f0604c66754aecc81e96ea35231221e31
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-07-11 15:13:31 +0000

    Create skull.sh

---
+++ api/troll/skull.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+clear
+
+echo -e "\033[0;32m     NO!                          MNO!    
+     MNO!!     [DUCKSPLOIT]        MNNOO!
+   MMNO!                           MNNOO!!
+ MNOONNOO!   MMMMMMMMMMPPPOII!   MNNO!!!!
+ !O! NNO! MMMMMMMMMMMMMPPPOOOII!! NO!
+       ! MMMMMMMMMMMMMPPPPOOOOIII! !
+        MMMMMMMMMMMMPPPPPOOOOOOII!!
+        MMMMMOOOOOOPPPPPPPPOOOOMII!
+        MMMMM..    OPPMMP    .,OMI!
+        MMMM::   o.,OPMP,.o   ::I!!
+          NNM:::.,,OOPM!P,.::::!!
+         MMNNNNNOOOOPMO!!IIPPO!!O!
+         MMMMMNNNNOO:!!:!!IPPPPOO!
+          MMMMMNNOOMMNNIIIPPPOO!!
+             MMMONNMMNNNIIIOO!
+           MN MOMMMNNNIIIIIO! OO
+          MNO! IiiiiiiiiiiiI OOOO
+     NNN.MNO!   O!!!!!!!!!O   OONO NO!
+    MNNNNNO!    OOOOOOOOOOO    MMNNON!
+      MNNNNO!    PPPPPPPPP    MMNON!
+         OO!                   ON!"
