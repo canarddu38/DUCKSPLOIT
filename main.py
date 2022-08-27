d2bd2a8b5fe571ee5a2eec3977248e9997ad6044
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-08-27 23:35:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -65,16 +65,19 @@ suggest: suggest an idea to our developpers```""", color=0x00ff44)
         elif message.content.startswith('ds!gcreate'):
            fullmessage = message.content.replace("ds!gcreate ", "").split(" ")
            if len(fullmessage) == 1:
-               prize = fullmessage[0]
-               embed = discord.Embed(title=":tada: Giveaway! :tada:", description="Prize: "+prize+"\nHosted by "+message.author.name)
-               embed.set_footer(text="React with :tada: for a chance to win a prize!")
-               embed.color=0x00ff44
+               if message.author.guild_permissions.administrator:
+                   prize = fullmessage[0]
+                   embed = discord.Embed(title=":tada: Giveaway! :tada:", description="Prize: "+prize+"\nHosted by "+message.author.mention)
+                   embed.set_footer(text="React with 🎉 for a chance to win a prize!")
+                   embed.color=0x00ff44
                
-               channelid = self.get_channel(961337408375369768)
-               msg = await channelid.send(embed=embed)
-               await msg.add_reaction("✔️") 
+                   channelid = self.get_channel(961337408375369768)
+                   msg = await channelid.send(embed=embed)
+                   await msg.add_reaction("🎉") 
 
-               await message.channel.send("Giveaway created :tada:")
+                   await message.channel.send("Giveaway created :tada:")
+               else:
+                   message.channel.send("✖️ error")
            else:
                message.channel.send("Usage: ds!gcreate <prize>")
 
