c77e76719fbef202becd28d754f7c04b0ecafcaf
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2023-01-31 17:43:32 +0100

    Add files via upload
    Signed-off-by: canarddu38 <71982379+canarddu38@users.noreply.github.com>

--- docs/download/download.js
+++ docs/download/download.js
@@ -16,6 +16,10 @@ function onload()
 		document.body.innerHTML+="<div class='snowflake'></div>";
 	}
 	
+	document.getElementById('linuxdownload').innerHTML+="<select name='version_lin' id='version_lin'>";
+	document.getElementById('windowsdownload').innerHTML+="<select name='version_win' id='version_win'>";
+	document.getElementById('androiddownload').innerHTML+="<select name='version_and' id='version_and'>";
+	
 	//versions
 	versions = httpGet("https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/versions.txt");
 	var linux_ver = versions.substring(
@@ -38,6 +42,7 @@ function onload()
 		else
 		{
 			temp = versions_lin_list[i].split("|");
+			temp[0]="Version: "+temp[0];
 			if(download_url_lin=="")
 			{
 				download_url_lin=temp[1];
@@ -48,7 +53,7 @@ function onload()
 			}
 		}
 	}
-	document.getElementById('linuxdownload').innerHTML+="<a onclick='window.location=`"+download_url_lin+"`' class='u-btn u-button-style u-palette-2-base u-btn-1'>Download</a>";
+	document.getElementById('linuxdownload').innerHTML+="<a onclick='window.location=`"+download_url_lin+"`' style='background-color: #e3c032' class='u-btn u-button-style u-palette-2-base u-btn-1'>Download</a>";
 	
 	//windows
 	versions_win_list = windows_ver.split("\n");
@@ -58,6 +63,7 @@ function onload()
 		else
 		{
 			temp = versions_win_list[i].split("|");
+			temp[0]="Version: "+temp[0];
 			if(download_url_win=="")
 			{
 				download_url_win=temp[1];
@@ -68,7 +74,7 @@ function onload()
 			}
 		}
 	}
-	document.getElementById('windowsdownload').innerHTML+="<a onclick='window.location=`"+download_url_win+"`' class='u-btn u-button-style u-palette-2-base u-btn-1'>Download</a>";
+	document.getElementById('windowsdownload').innerHTML+="<a onclick='window.location=`"+download_url_win+"`' style='background-color: #599ce0' class='u-btn u-button-style u-palette-2-base u-btn-1'>Download</a>";
 	
 	//android
 	versions_and_list = android_ver.split("\n");
@@ -78,6 +84,7 @@ function onload()
 		else
 		{
 			temp = versions_and_list[i].split("|");
+			temp[0]="Version: "+temp[0];
 			if(download_url_and=="")
 			{
 				download_url_and=temp[1];
@@ -88,5 +95,5 @@ function onload()
 			}
 		}
 	}
-	document.getElementById('androiddownload').innerHTML+="<a onclick='window.location=`"+download_url_and+"`' class='u-btn u-button-style u-palette-2-base u-btn-1'>Download</a>";
+	document.getElementById('androiddownload').innerHTML+="<a onclick='window.location=`"+download_url_and+"`' style='background-color: #a4c639' class='u-btn u-button-style u-palette-2-base u-btn-1'>Download</a>";
 }
