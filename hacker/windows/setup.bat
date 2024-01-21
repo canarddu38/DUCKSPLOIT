dace5e86240582650b10b71e1ed384156a2a1856
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2024-01-21 23:32:38 +0100

    Update setup.bat
    Signed-off-by: canarddu38 <71982379+canarddu38@users.noreply.github.com>

--- hacker/windows/setup.bat
+++ hacker/windows/setup.bat
@@ -1,13 +1,12 @@
 @echo off
-REM DuckpvpTeam - 2022
+REM 4re5 group - 2024
 
-set version=1.0.9
+set version=%1
 
 mkdir %USERPROFILE%\DuckSploit
 cls
 echo Downloading...
-powershell.exe -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/canarddu38/DUCKSPLOIT/blob/root/hacker/windows/ds.exe?raw=true', '%USERPROFILE%\DuckSploit\ds.exe')"
-powershell.exe -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/canarddu38/DUCKSPLOIT/blob/root/hacker/windows/ds.exe?raw=true', '%USERPROFILE%\DuckSploit\ds.exe')
+powershell.exe -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/canarddu38/DUCKSPLOIT/raw/root/hacker/windows/versions/ds-%version%.exe', '%USERPROFILE%\DuckSploit\ds.exe')
 powershell.exe -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/canarddu38/DUCKSPLOIT/raw/root/images/icon.ico?raw=true', '%USERPROFILE%\DuckSploit\icon.ico')
 powershell.exe -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/canarddu38/DUCKSPLOIT/raw/root/hacker/windows/Mono.Nat.dll?raw=true', '%USERPROFILE%\DuckSploit\Mono.Nat.dll')
 
@@ -16,7 +15,7 @@ powershell Add-MpPreference -ExclusionPath "%userprofile%\DuckSploit\"
 
 (
 echo @echo off
-echo REM DuckpvpTeam - 2022
+echo REM 4re5 group - 2024
 echo call %USERPROFILE%\DuckSploit\ds.exe
 )>"%windir%\System32\ds.bat"
 
