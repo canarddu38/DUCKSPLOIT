fb20440564b8fe308a3dbb7355afad7007258932
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2024-01-23 16:35:56 +0100

    Add files via upload
    Signed-off-by: canarddu38 <71982379+canarddu38@users.noreply.github.com>

---
+++ hacker/windows/versions/AssemblyInfo.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DuckSploit")]
+[assembly: AssemblyDescription("Awesome Remote Administration Tool for any platform")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("4re5 group")]
+[assembly: AssemblyProduct("DuckSploit")]
+[assembly: AssemblyCopyright("4re5 group ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+//In order to begin building localizable applications, set
+//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
+//inside a <PropertyGroup>.  For example, if you are using US english
+//in your source files, set the <UICulture> to en-US.  Then uncomment
+//the NeutralResourceLanguage attribute below.  Update the "en-US" in
+//the line below to match the UICulture setting in the project file.
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyVersion("1.1.4")]
+[assembly: AssemblyFileVersion("1.1.4")]
+[assembly: NeutralResourcesLanguage("en")]
