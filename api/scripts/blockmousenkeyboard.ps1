870b93ae5541ec30987bfd30319300d59329fb58
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2024-01-29 16:47:21 +0100

    Update blockmousenkeyboard.ps1
    Signed-off-by: canarddu38 <71982379+canarddu38@users.noreply.github.com>

--- api/scripts/blockmousenkeyboard.ps1
+++ api/scripts/blockmousenkeyboard.ps1
@@ -1,3 +1,5 @@
+# WARNING: THIS CODE CAN BREAK YOUR WINDOWS REGISTRY
+
 #Run As Administrator
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
